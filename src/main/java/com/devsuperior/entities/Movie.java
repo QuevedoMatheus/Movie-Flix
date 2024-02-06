ec79b426ee59deb5552a7f60a55b9895b9f10849
unknown <matheusquevedobio@gmail.com>
2024-02-05 23:26:37 -0300

    Iniciando projeto, adicionando as entidades e a dependencias

---
+++ src/main/java/com/devsuperior/entities/Movie.java
@@ -0,0 +1,111 @@
+package com.devsuperior.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tb_movie")
+public class Movie implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String subTitle;
+	private Integer year;
+	private String imgUri;
+	
+	@Column(name="synopsis",columnDefinition="TEXT")
+	private String synopsis;
+	
+	public Movie() {
+		
+	}
+
+	public Movie(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis) {
+		this.id = id;
+		this.title = title;
+		this.subTitle = subTitle;
+		this.year = year;
+		this.imgUri = imgUri;
+		this.synopsis = synopsis;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUri() {
+		return imgUri;
+	}
+
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}

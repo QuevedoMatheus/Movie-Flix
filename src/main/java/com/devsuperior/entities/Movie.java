8ec696f113fbee784d82d8df3d33e6ffb8e85f56
unknown <matheusquevedobio@gmail.com>
2024-02-06 21:07:39 -0300

    ajuste de configuração em pom.xml e application.properties para funcionamento do banco de dados h2

--- src/main/java/com/devsuperior/entities/Movie.java
+++ src/main/java/com/devsuperior/entities/Movie.java
@@ -1,6 +1,8 @@
 package com.devsuperior.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -8,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,17 +31,25 @@ public class Movie implements Serializable {
 	@Column(name="synopsis",columnDefinition="TEXT")
 	private String synopsis;
 	
+	@ManyToOne
+	@JoinColumn(name= "genre_id")
+	private Genre genre;
+	
+	@OneToMany(mappedBy = "movie")
+	private List<Review> reviews = new ArrayList<>();
+	
 	public Movie() {
 		
 	}
 
-	public Movie(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis) {
+	public Movie(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis, Genre genre) {
 		this.id = id;
 		this.title = title;
 		this.subTitle = subTitle;
 		this.year = year;
 		this.imgUri = imgUri;
 		this.synopsis = synopsis;
+		this.genre = genre;
 	}
 
 	public Long getId() {
@@ -85,6 +98,18 @@ public class Movie implements Serializable {
 
 	public void setSynopsis(String synopsis) {
 		this.synopsis = synopsis;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
 	}
 
 	@Override
